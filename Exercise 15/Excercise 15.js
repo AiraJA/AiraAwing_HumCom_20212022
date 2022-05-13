9bb99866d7ca7cd9235122dd769b103f17883da8
Aira Joy Awing <98304492+AiraJA@users.noreply.github.com>
2022-05-13 21:21:23 +0800

    Add files via upload

---
+++ Exercise 15/Excercise 15.js
@@ -0,0 +1,19 @@
+var firstName= "Aira Joy Awing";
+var age= "21";
+var phone= "09630240620";
+var email= "20163238@s.ubaguio.edu";
+var address= "BR2 Lancaster New City, Cavite";
+var program= "Information technology";
+var id="20163238";
+var year= "2nd Year";
+var section= "IAA2"
+
+console.log("Name: " + firstName);
+console.log("Age: " + age);
+console.log("Phone Number:" + phone);
+console.log("Email Address: " + email);
+console.log("Mailing Address: " + address);
+console.log("Program: "+ program);
+console.log("ID Number: " + id);
+console.log("Year Level: "+ year);
+console.log("Section: " + section);
